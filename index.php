0ba6a5e4c7cad899b202c21a8a02e121f9b6c4b9
alanlasco <61526254+alanlasco@users.noreply.github.com>
2024-03-22 20:08:13 -0300

    Adding list of poems, fixing navbar

--- index.php
+++ index.php
@@ -7,13 +7,16 @@ require './parts/login-checker.php'
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+        <link rel="stylesheet" href="./css/mostrar-poemas.css">
+
     <title>Form</title>
 </head>
 <body>
     <?php
-    require './parts/navbar.php'
+    require './parts/navbar.php';
+    require './parts/mostrar-poemas.php';
     ?>
-    <h1>is working</h1>
+    
     
 </body>
 </html>
---
+++ parts/mostrar-poemas.php
@@ -0,0 +1,39 @@
+<?php
+require './config/config.php';
+require 'login-checker.php';
+session_start();
+$sql="SELECT fecha, id_poema, nombre, poema FROM poemas";
+
+$resultado = mysqli_query($conexion, $sql);
+
+
+// En el caso de que la consulta traiga una respuesta, entonces la decodifico
+if (mysqli_num_rows($resultado)!=0) {
+  $resultado = mysqli_fetch_all($resultado, MYSQLI_ASSOC);
+}else{
+  $error = "Error";
+}
+//var_dump($resultado);
+?>
+ <br><br>
+ <table>
+  <tr>
+    <th>ID</th>
+    <th>Nombre</th>
+    <th>Fecha</th>
+  </tr>
+   <?php for($i=0; $i < count($resultado); $i++){ ?>
+    <tr class="poema"><td>
+     <?php echo $resultado[$i]['id_poema'].' ' ?>
+    </td>
+    <td>
+     <?php echo $resultado[$i]['nombre'].' ' ?>
+    </td>
+    <td>
+    <?php
+    $fecha = date_create($resultado[$i]['fecha']);
+    echo date_format($fecha, 'd-m-Y');
+    ?>        
+    </td>
+    </tr>
+       <?php } ?>
