7b92596ea6cf676a813ef60a5400757de26389e8
alanlasco <61526254+alanlasco@users.noreply.github.com>
2024-03-30 19:07:23 -0300

    adding styles for add-poem form

--- parts/add-poem.php
+++ parts/add-poem.php
@@ -7,7 +7,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <link rel="stylesheet" href="formulario.css">
+    <link rel="stylesheet" href="../css/add-poem.css">
     <link rel="stylesheet" href="inputs.css">
     <title>Agregar</title>
 </head>
@@ -15,29 +15,31 @@
     <?php
     require 'navbar.php';
     ?>
-      <h1 class='titulo'>Agregar Elemento</h1>
-  <form class='formulario' action="add-poem-process.php" method="post">
-  <div class="input-wrapper"> 
-  <label for="nombre">Nombre poema:</label>
-    <br>
+      <h1 class='heading'>Agregar Poema</h1>
+  <form class="form-container" action="add-poem-process.php" method="post">
+
+
+  <div class="inputs"> 
+    <div>
+  <label class="label-inline" for="nombre">Nombre poema:</label>
     <input class="input" type="text" id="nombre" name="nombre" required>
-    <br>
     </div>
     <div>
-    <label for="poema">poema:</label>
-    <br>
-    <textarea name="poema" id="poema" cols="30" rows="10" required>
+    <label for="favorita">Es un poema favorito? 1=Si 2=No  </label>
+    <input class="input-number" type="number" min="1" max="2" name="favorita" id="favorita" required>
+    </div>
+    <div>
+    <input class="submit-btn" type="submit" value="Agregar">
+    </div>
+</div>
+
+    <div class="text-area">
+    <label class="poem-name" for="poema">poema:</label>
+    <textarea class="poem" name="poema" id="poema" cols="30" rows="10" required>
     </textarea>
     </div>
-    <br>
-    <div class="input-wrapper">
-    <label for="favorita">Es un poema favorito? 1=Si 2=No  </label>
-    <input class="input" type="number" name="favorita" id="favorita" required>
-    </div>
-    <br>
-    <div>
-    <input type="submit" value="Agregar">
-    </div>
+
+
   </form>
     
 </body>
